d07f9483fd55f23a6aa7b1db04f7593450f67098
Niharika100 <56485421+Niharika100@users.noreply.github.com>
2021-06-09 13:06:24 +0530

    Update HomeScreen.js
    Added comments

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -10,8 +10,11 @@ import {
 
 
 const HomeScreen = ({ navigation }) => {
+  
+  // useDispatch to call the reducer for the redux store update
   const dispatch = useDispatch();
 
+  // function to fetch the list from server
   const getUserList = async () => {
     const options = { method: 'GET' }
     const userList = await fetch('https://reqres.in/api/users?page=2', options)
@@ -19,6 +22,7 @@ const HomeScreen = ({ navigation }) => {
       .then(res => { return res })
 
     if (userList) {
+      // if the fetch is success, the data will be updated to the redux store and navigate the app to the User List View screen
       dispatch({ type: 'loadData', payload: userList })
       navigation.navigate('UserList');
     }
@@ -26,6 +30,8 @@ const HomeScreen = ({ navigation }) => {
 
   return (
     <View style={{padding: PR.getPixelSizeForLayoutSize(24) / PR.get()}}>
+      // button to load the user list
+      // TouchableOpacity is used to add styling
       <TouchableOpacity style= {styles.buttonContainer} onPress={() => { getUserList() }}>
         <Text style={styles.textContainer}>Add Load List</Text>
       </TouchableOpacity>
